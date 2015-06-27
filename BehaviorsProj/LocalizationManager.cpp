d55d58d81ed43d241539b8ba49e7913da2c6582f
MichaelRo <michroyt@gmail.com>
2015-06-27 10:51:28 -0700

    LocalizationManager checkin

---
+++ BehaviorsProj/LocalizationManager.cpp
@@ -0,0 +1,53 @@
+/*
+ * LocalizationManager.cpp
+ *
+ *  Created on: Jun 27, 2015
+ *      Author: Noa Even Tsur, Itay Desalto, Michael Roytman
+ */
+
+#include "LocalizationManager.h"
+
+LocalizationManager::LocalizationManager(Structs::Location startLocation, Map * map) {
+	_map = map;
+	_particles.push_front(*(new Particle(startLocation, map)));
+}
+
+LocalizationManager::~LocalizationManager() {
+	delete _map;
+}
+
+void LocalizationManager::updateParticles(Structs::Location destination, vector<float> laserScan) {
+	list<Particle>::iterator particlesIterator = _particles.begin();
+
+	while (particlesIterator != _particles.end()) {
+		float currentParticleBelief = particlesIterator->update(destination, laserScan);
+
+		if (currentParticleBelief < BELIEF_THRESHOLD) {
+			_particles.erase(particlesIterator++);
+		} else {
+			// Check if this works on the previous particle
+			particlesIterator++->createDescendantParticles(PARTICLES_AMOUNT - _particles.size());
+		}
+	}
+}
+
+Particle LocalizationManager::getHighestBeliefParticle() {
+	list<Particle>::iterator particlesIterator = _particles.begin();
+	Particle * highestBeliefParticle = particlesIterator.operator ->();
+
+	for (particlesIterator = _particles.begin(); particlesIterator != _particles.end(); ++particlesIterator) {
+		if (particlesIterator->getBelief() > highestBeliefParticle->getBelief())
+			highestBeliefParticle = particlesIterator.operator ->();
+	}
+
+	return *highestBeliefParticle;
+}
+
+Structs::Location LocalizationManager::getProbableLocation() {
+	return getHighestBeliefParticle().getLocation();
+}
+
+void LocalizationManager::createNewParticles() {
+	// Decide who randoms the location - the LocalizationManager or the Particle itself
+	getHighestBeliefParticle().createDescendantParticles(PARTICLES_AMOUNT - _particles.size());
+}
---
+++ BehaviorsProj/LocalizationManager.h
@@ -0,0 +1,39 @@
+/*
+ * LocalizationManager.h
+ *
+ *  Created on: Jun 27, 2015
+ *      Author: Noa Even Tsur, Itay Desalto, Michael Roytman
+ */
+
+#ifndef LOCALIZATIONMANAGER_H_
+#define LOCALIZATIONMANAGER_H_
+
+#include <list>
+
+#include "Map.h"
+#include "Particle.h"
+#include "Structs.h"
+
+using namespace std;
+
+class LocalizationManager {
+private:
+	static const int PARTICLES_AMOUNT = 100;
+	static const int BELIEF_THRESHOLD = 0.6; // 0.6 / 0.25
+//	static const int STANDARD = 0.7 - maybe we need a high threshold
+
+	list<Particle> _particles;
+	Map* _map;
+
+	Particle getHighestBeliefParticle();
+
+public:
+	LocalizationManager(Structs::Location startLocation, Map * map);
+	virtual ~LocalizationManager();
+
+	void updateParticles(Structs::Location destination, vector<float> laserScan);
+	Structs::Location getProbableLocation();
+	void createNewParticles();
+};
+
+#endif /* LOCALIZATIONMANAGER_H_ */
