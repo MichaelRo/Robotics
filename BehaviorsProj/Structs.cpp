b0595257084de869fe2b945ddc8db97631b5de1d
Michael Roytman <michroyt@gmail.com>
2015-06-30 22:53:11 +0300

    Update Structs.cpp

--- BehaviorsProj/Structs.cpp
+++ BehaviorsProj/Structs.cpp
@@ -7,34 +7,18 @@
 
 #include "Structs.h"
 
-Structs::Size::Size(int width, int height) {
-	this->width = width;
-	this->height = height;
-}
-
 Structs::Location::Location(float x, float y, float yaw) {
 	this->x = x;
 	this->y = y;
 	this->yaw = yaw;
 }
 
+Structs::Size::Size(int width, int height) {
+	this->width = width;
+	this->height = height;
+}
+
 Structs::ConfigurationProperty::ConfigurationProperty(string token, string value) {
 	this->token = token;
 	this->value = value;
 }
-
-Structs::Point::Point() {
-	x = 0;
-	y = 0;
-}
-
-Structs::Point::Point (int xCoord, int yCoord) {
-	x = xCoord;
-	y = yCoord;
-}
-
-Structs::Point::Point (Point *p) {
-	x = p->x;
-	y = p->y;
-}
-
