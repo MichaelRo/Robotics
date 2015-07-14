b6d6b6c85037dc25da874851c86498bcb443b7fd
MichaelRo <michroyt@gmail.com>
2015-07-14 16:06:14 -0700

    Igal

--- BehaviorsProj/LocalizationManager.cpp
+++ BehaviorsProj/LocalizationManager.cpp
@@ -18,7 +18,8 @@ LocalizationManager::LocalizationManager(Structs::Location startLocation, MapFor
 	// Initializing the particles list by the PARTICLES_AMOUNT value
 	_particles.push_back(Particle(startLocation, (float) 1, map));
 
-	getHighestBeliefParticle()->createDescendantParticles(PARTICLES_AMOUNT - 1);
+	list<Particle> descendantParticles = getHighestBeliefParticle()->createDescendantParticles(PARTICLES_AMOUNT - 1);
+	_particles.insert(_particles.end(), descendantParticles.begin(), descendantParticles.end());
 
 //	for (int particleCreationIndex = 0; particleCreationIndex < (PARTICLES_AMOUNT - 1); particleCreationIndex++)
 //		_particles.push_back(Particle(startLocation, (float) 0.1, _map));
